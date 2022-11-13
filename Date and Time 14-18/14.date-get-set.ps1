a735949a159044ef99bf4fcc60661ee1a5eaf66b
Jignesh Rawal <jignesh.rawal@outlook.com>
2022-11-14 00:00:52 +0100

    Get and Set Date

---
+++ Date and Time 14-18/14.date-get-set.ps1
@@ -0,0 +1,12 @@
+﻿#This file may change your system current date and time.
+#Be vigilant and under clock setting click on Sync Now to sync with Internet Time Zone to avoid issues with your computer.
+Get-Date 
+
+#Should run the powershell script with Admin Privileges to set the date.
+Set-Date -Date (Get-Date).AddDays(1)
+
+Get-Date
+
+#Get back to todays date
+
+Set-Date -Date (Get-Date).AddDays(2)
