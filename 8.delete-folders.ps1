d6773da266a2a410c0480cca2f7828b2448412ce
Jignesh Rawal <jignesh.rawal@outlook.com>
2022-11-13 17:04:51 +0100

    Delete folder if it exists

---
+++ 8.delete-folders.ps1
@@ -0,0 +1,9 @@
+﻿#Create a new folder
+New-Item -Path 'Folder1' -ItemType Directory
+
+#check if folder exists
+if(Test-Path -Path 'Folder1'){
+Remove-Item 'Folder1'
+}else{
+Write-host 'Folder1 to be deleted does not exist'
+}
