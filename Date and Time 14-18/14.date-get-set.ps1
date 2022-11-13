7bdb18241dcbc85c750c971729017ea0d40c5b73
Jignesh Rawal <jignesh.rawal@outlook.com>
2022-11-14 00:21:30 +0100

    Get and Set Date - few more functions for learning

--- Date and Time 14-18/14.date-get-set.ps1
+++ Date and Time 14-18/14.date-get-set.ps1
@@ -10,3 +10,17 @@ Get-Date
 #Get back to todays date
 
 Set-Date -Date (Get-Date).AddDays(2)
+
+#To Print only the date and not the time.
+
+Get-Date -DisplayHint Date
+
+#To Print only the time and not the date.
+
+Get-Date -DisplayHint Time
+
+#Get the short format of the only date
+(Get-Date).ToShortDateString();
+
+#Check if the Day light Saving is on in your current location.
+(Get-Date).IsDaylightSavingTime();
