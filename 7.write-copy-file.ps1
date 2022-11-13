a5dda082569ae64f30e23bdb755c4e82bf7228a4
Jignesh Rawal <jignesh.rawal@outlook.com>
2022-11-13 14:33:03 +0100

    Write a file and copy the contents of the file to a new file

---
+++ 7.write-copy-file.ps1
@@ -0,0 +1,6 @@
+﻿'The new file will be created. This would be the content of the new file' | Out-File -FilePath 'PSCreatedfile.txt'
+if(Test-Path -Path 'PSCreatedfile.txt'){
+Copy-Item -Path 'PSCreatedfile.txt' -Destination 'FileCopied.txt'
+}else{
+Write-Host 'File Does not exists'
+}
