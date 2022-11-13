4511410bd7aaa04f0224fc6902a8aa5a5392c479
Jignesh Rawal <jignesh.rawal@outlook.com>
2022-11-13 13:43:16 +0100

    Copy Contents of the folder to another folder

---
+++ 6.copy-folder.ps1
@@ -0,0 +1,8 @@
+﻿$folderToBeCopiedPath = 'Folder1'
+$newFolderName = 'Folder2'
+
+#Create a new folder
+New-Item -Path 'Folder2' -ItemType Directory
+
+#Below will copy contents of the folder and not the entire folder.
+Copy-Item -Path ($folderToBeCopiedPath + "\*") -Destination $newFolderName -PassThru
