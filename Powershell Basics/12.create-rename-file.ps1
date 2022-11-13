578999db50d99038418d755734dedcea019e8e55
Jignesh Rawal <jignesh.rawal@outlook.com>
2022-11-13 23:17:31 +0100

    Create and Rename file

---
+++ Powershell Basics/12.create-rename-file.ps1
@@ -0,0 +1,7 @@
+﻿New-Item -Path 'newFile.txt' -ItemType File
+
+if(Test-Path -Path 'newFile.txt'){
+Rename-Item 'newFile.txt' 'renamedNewFile.txt'
+}else{
+Write-Host 'newFile.txt to be renamed does not exists'
+}
