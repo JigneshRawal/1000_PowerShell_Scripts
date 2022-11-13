d0fab19340373743677abf5cc87b3f5b2f46aedc
Jignesh Rawal <jignesh.rawal@outlook.com>
2022-11-13 14:05:16 +0100

    Recursively copy the contents of the folder

--- 6.copy-folder.ps1
+++ 6.copy-folder.ps1
@@ -6,3 +6,6 @@ New-Item -Path 'Folder2' -ItemType Directory
 
 #Below will copy contents of the folder and not the entire folder.
 Copy-Item -Path ($folderToBeCopiedPath + "\*") -Destination $newFolderName -PassThru
+
+#use - recurse to copy the contents of the inner folder as well
+Copy-Item -Path ($folderToBeCopiedPath + "\*") -Destination $newFolderName -Recurse
