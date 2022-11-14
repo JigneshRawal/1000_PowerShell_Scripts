2e3d260366dd61705f8a0692b974d7ace9646d0d
Jignesh Rawal <jignesh.rawal@outlook.com>
2022-11-14 23:06:29 +0100

    Group-Object Cmdlet - Measure-object

---
+++ Advanced-Cmdlets/16.measure-object.ps1
@@ -0,0 +1,9 @@
+﻿#create a file.
+
+New-Item -Path 'test.txt' -ItemType File
+
+'This is the real content of the file.We will use this file to count stats' | Out-File 'test.txt'
+
+Get-Content 'test.txt' | Measure-Object -Character -Line -Word
+
+Get-ChildItem | Measure-Object
