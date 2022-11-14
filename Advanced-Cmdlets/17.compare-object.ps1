f4579dcf3d5c7ce819a5bf6f3ac98cf703016d7e
Jignesh Rawal <jignesh.rawal@outlook.com>
2022-11-14 23:41:38 +0100

    Compare-object get comparison of the content of 2 files

---
+++ Advanced-Cmdlets/17.compare-object.ps1
@@ -0,0 +1,11 @@
+﻿New-Item -Path 'test1.txt' -ItemType File
+'Contents of the first file test1.txt
+This is the next line' | Out-File 'test1.txt'
+
+New-Item -Path 'test2.txt' -ItemType File
+'Contents of the second file test2.txt
+This is the next line' | Out-File 'test2.txt'
+
+Compare-Object -ReferenceObject (Get-Content 'test1.txt') -DifferenceObject (Get-Content 'test2.txt')
+
+Compare-Object -ReferenceObject (Get-Content 'test1.txt') -DifferenceObject (Get-Content 'test2.txt') -IncludeEqual
